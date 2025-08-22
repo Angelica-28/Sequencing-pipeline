50abdcc871e7027c90908c4fb79abab59c8dc52a
Angelica <angelica.oian@unipd.it>
2025-08-22 16:37:18 +0200

    Update prepare_ref.sh

--- Script/prepare_ref.sh
+++ Script/prepare_ref.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-# === CONFIGURAZIONE ===
-REF="Bos_taurus.ARS-UCD1.2.dna.toplevel.fa"  # <-- cambia se il tuo file ha nome diverso
-GATK="/home/angelica/WGS/Programs/tools/gatk-4.5.0.0/gatk"
-SAMTOOLS="/home/angelica/WGS/Programs/tools/samtools-1.19.2/samtools"
-BWA="/home/angelica/WGS/Programs/tools/bwa-0.7.17/bwa"
+# === CONFIGURATION ===
+REF="Bos_taurus.ARS-UCD1.2.dna.toplevel.fa"  # <-- change if necessary
+GATK="/home/usr/project/Programs/tools/gatk-4.5.0.0/gatk"
+SAMTOOLS="/home/usr/project/Programs/tools/samtools-1.19.2/samtools"
+BWA="/home/usr/project/Programs/tools/bwa-0.7.17/bwa"
 # === CHECK FILE ===
 if [ ! -f "$REF" ]; then
-  echo "❌ Reference file $REF non trovato. Controlla il nome."
+  echo "❌ Reference file $REF not found. Check name."
   exit 1
 fi
 
-echo "✅ Inizio indicizzazione della reference: $REF"
+echo "✅ Indexing: $REF"
 
 # 1. Samtools faidx
-echo "→ Genero file .fai (samtools faidx)..."
+echo "→ Generating file .fai (samtools faidx)..."
 $SAMTOOLS faidx "$REF"
 
 # 2. Picard/GATK dictionary
-echo "→ Genero file .dict (CreateSequenceDictionary)..."
+echo "→ Generating file .dict (CreateSequenceDictionary)..."
 $GATK CreateSequenceDictionary -R "$REF"
 
 # 3. BWA index
-echo "→ Genero index per BWA..."
+echo "→ Generating index per BWA..."
 $BWA index "$REF"
 
-echo "✅ Tutti i file di indicizzazione generati con successo:"
+echo "✅ All index file generated:"
 echo "  - ${REF}.fai"
 echo "  - ${REF%.fa}.dict"
 echo "  - file .amb, .ann, .bwt, .pac, .sa (per BWA)"
